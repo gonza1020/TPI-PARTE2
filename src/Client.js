233758e5d39bf29af73c597ea79db95fc023e527
gonza1020 <gonzalozoloaga44@gmail.com>
2021-11-23 14:52:34 -0300

    CLient class

---
+++ src/Client.js
@@ -0,0 +1,12 @@
+// CLient Constructor
+export class Client {
+    constructor(apeYnom,dni, cuil, fecNac,telefono,email) {
+        this.apeYnom = apeYnom;
+        this.dni = dni;
+        this.cuil= cuil;        t
+        this.fecNac = fecNac;
+        this.telefono = telefono;
+        this.email = email;
+        // COnstructor
+    }
+}
